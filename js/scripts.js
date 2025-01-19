55aeea5ba1ac2df46d689ff1825b32e8bb75fdcc
Extkoer <extkoer102@gmail.com>
2025-01-19 10:58:44 +0500

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
 function transitionToSection(section) {
   const heroSection = document.getElementById('hero');
   const mainContent = document.getElementById('main-content');
+  const sidebar = document.getElementById('sidebar');
 
   heroSection.style.display = 'none';
   mainContent.style.display = 'block';
 
-  // Show specific section
   document.querySelectorAll('main > section').forEach(sec => sec.classList.add('hidden'));
   document.getElementById(`section-${section}`).classList.remove('hidden');
 
-  // Show Sidebar
-  document.getElementById('sidebar').classList.remove('hidden');
+  sidebar.classList.remove('hidden');
 }
 
 function showSection(section) {
@@ -23,6 +22,7 @@ function goHome() {
   document.getElementById('main-content').style.display = 'none';
   document.getElementById('sidebar').classList.add('hidden');
 }
+
 
 
 
